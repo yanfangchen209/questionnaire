0e10b10b0dffd0d801e32615ec95dc7f99ab4f1a
yanfang chen <juliechen209@gmail.com>
2023-07-24 00:00:49 -0500

    add draft html css js servlet

---
+++ src/main/java/questionnaire/QuestionnaireServlet.java
@@ -0,0 +1,39 @@
+package questionnaire;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class QuestionnaireServlet
+ */
+public class QuestionnaireServlet extends HttpServlet {
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//String firstName = request.getParameter("firstName" + "lastName");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String name = firstName + " " + lastName;
+		PrintWriter out = response.getWriter();
+		out.println(name + ", thank you for submmission!");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
